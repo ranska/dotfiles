d22cfc12fc89e358179f2f9578d66e1e64b693fe
ranska <go@rubyrock.me>
2013-05-19 17:36:15 +0200

    création script install

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/gvimrc ~/.gvimrc
+ln -s ~/dotfiles/irbrc ~/.irbrc
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/ctags ~/.ctags
+ln -s ~/dotfiles/jshintrc ~/.jshintrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
